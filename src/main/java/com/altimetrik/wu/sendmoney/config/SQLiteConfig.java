f5e9c2b7f3a3b0f4321e0f802427eb3aba418ce9
anil-altimetrik <aingle@altimetrik.com>
2023-08-21 18:47:51 +0530

    Added db details

---
+++ src/main/java/com/altimetrik/wu/sendmoney/config/SQLiteConfig.java
@@ -0,0 +1,21 @@
+package com.altimetrik.wu.sendmoney.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+@Configuration
+public class SQLiteConfig {
+    @Bean
+    public DataSource dataSource() {
+        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.sqlite.JDBC");
+        dataSource.setUrl("jdbc:sqlite:D:\\User\\aingle\\send-money\\send-money.db");
+        //dataSource.setUsername("sa");
+        //dataSource.setPassword("sa");
+
+
+        return dataSource;
+    }
+}
