148c4dcfd6d407d98b9bb8dcb1ddbdf07c665a80
anil-altimetrik <aingle@altimetrik.com>
2023-08-28 14:31:24 +0530

    Added mtcn

--- src/main/java/com/altimetrik/wu/sendmoney/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/altimetrik/wu/sendmoney/service/impl/TransactionServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class TransactionServiceImpl implements TransactionService {
@@ -39,9 +40,44 @@ public class TransactionServiceImpl implements TransactionService {
         txn.setTransactionAmount(transactionRequest.getTransactionAmount());
         txn.setTransactionStatus("Initiated");
         txn.setCreatedBy(transactionRequest.getCreatedBy());
-        txn.setMtcn(transactionRequest.getMtcn());
+        txn.setMtcn(Long.valueOf(generateMtcn("56")));
 
         txn = transactionRepository.save(txn);
         return txn.getMtcn();
+    }
+
+    private int getCheckDigit(String number) {
+        int sum = 0;
+        for (int i = 0; i < number.length(); i++) {
+
+            int digit = Integer.parseInt(number.substring(i, (i + 1)));
+
+            if ((i % 2) == 0) {
+                digit = digit * 2;
+                if (digit > 9) {
+                    digit = (digit / 10) + (digit % 10);
+                }
+            }
+            sum += digit;
+        }
+
+        int mod = sum % 10;
+        return ((mod == 0) ? 0 : 10 - mod);
+    }
+
+    private String generateMtcn(String startWith) {
+        Random random = new Random(System.currentTimeMillis());
+        //int randomNumberLength = 16 - ("23037700041".length() + 1);
+        int randomNumberLength = 16 - (startWith.length() + 1);
+
+        StringBuilder builder = new StringBuilder(startWith);
+        for (int i = 0; i < randomNumberLength; i++) {
+            int digit = random.nextInt(10);
+            builder.append(digit);
+        }
+
+        int checkDigit = this.getCheckDigit(builder.toString());
+        builder.append(checkDigit);
+        return builder.toString();
     }
 }
